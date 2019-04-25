5d5d8f9aca1bbcdfbe5680d2f37679fce2e66ed6
Manuel Meraz <meraz.manuel@gmail.com>
2019-04-24 19:29:45 -0700

    removed main.cpp file that i commited by accident

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <algorithm>
-#include <array>
-
-int main(int /* argc */, char ** /* argv[] */) {
-  std::array<int, 1> arr{1};
-  std::sort(std::begin(arr), std::end(arr));
-  return 0;
-}
